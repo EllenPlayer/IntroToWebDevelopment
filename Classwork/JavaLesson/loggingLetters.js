d90ea3bd3e06b3d08447a69d528f590605f9ee8d
Ellen Player <ellenp123@outlook.com>
2020-07-21 21:36:57 +0100

    loggingLetters JavaScript and HTML code to practice loops

---
+++ Classwork/JavaLesson/loggingLetters.js
@@ -0,0 +1,12 @@
+/*
+Name: Ellen Player
+Date Created: 21/07/2020
+Most recent revision:
+*/
+var sentence="I love learning software development";
+
+for(var i=0; i<sentence.length;i++){
+		if(sentence[i] !==" "){
+		console.log(sentence[i]);
+		}
+}
